d9373698b37439ef5f6ee0c9bcb0ed0c386096ed
JonSno29 <109987633+JonSno29@users.noreply.github.com>
2022-12-05 15:55:12 -0700

    Added information to logout file

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,6 +1,15 @@
 const logout = async () => {
   // Send a POST request to the API endpoint /api/users/logout
+const response = await fetch('/api/users/logout', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' }
+    });
   
-};
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      alert(response.statusText);
+    }
+  };
 
 document.querySelector('#logout').addEventListener('click', logout);
