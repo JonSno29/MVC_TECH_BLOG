114c54728e34fb361c2940926c8248135e746ee0
JonSno29 <109987633+JonSno29@users.noreply.github.com>
2022-12-05 14:16:39 -0700

    added GET home routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,12 +1,39 @@
 const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Post, User, Comment } = require('../models');
 
 router.get('/', async (req, res) => {
-  //TODO: Find all projects in the database
-
-  //TODO: Serialize the data from sequelize query
-
-  //TODO: Render the homepage handlebars
+try {
+    const dbPostData = await Post.findAll({
+      attributes: ["id", "title", "content", "created_at"],
+      include: [
+        {
+          model: Comment,
+          attributes: [
+            "id",
+            "comment_text",
+            "post_id",
+            "user_id",
+            "created_at",
+          ],
+          include: {
+            model: User,
+            attributes: ["username"],
+          },
+        },
+        {
+          model: User,
+          attributes: ["username"],
+        },
+      ],
+    });
   
+ const posts = dbPostData.map(post => post.get({ plain: true }));
+    res.render("homepage", { posts, loggedIn: req.session.loggedIn });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 router.get('/login', async (req, res) => {
